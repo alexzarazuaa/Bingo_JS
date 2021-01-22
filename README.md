4264d612d7007d5df476f69e0997b868354ff9a3
Alex Zarazua <alexzarazua88@gmail.com>
2021-01-22 16:57:29 +0100

    starting readme

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-# curs2020_2021
-Exercises about javascript 
-
-#88cefa blau cel
-#cefa87 verd claret
-#fa87cd cyan
+ALEX ZARAZUA SOLER
--- bingo_twingo/src/utils/settings.js
+++
@@ -1,9 +0,0 @@
-export let Settings = {
-
-    "settings":{
-        "Speed":2,
-        "theme":"dark-wingo-theme",
-        "wait-seconds-after-linia":2
-    }
-
-}
