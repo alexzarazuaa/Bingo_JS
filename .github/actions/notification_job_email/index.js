e1ec330170e289a9867265ad42e810890df2ba38
Alex Zarazua <alexzarazua88@gmail.com>
2021-01-31 19:32:45 +0100

    notification_job

--- .github/actions/notification_job_email/index.js
+++ .github/actions/notification_job_email/index.js
@@ -17,11 +17,11 @@ let core = require("@actions/core");
     from: 'alexzarazua7email.test@gmail.com',// sender address
     to: core.getInput('WORK_CLASS_EMAIL'), // list of receivers
     subject: "Resultado del workflow ejecutado ✔", // Subject line
-    text: "Se ha realizado un push en la rama 'githubActions_improvement' que ha provocado la ejecución del workflow Bingo_Workflow con los siguientes resultados:\n\n- syntax_check_job: \n\n- test_execution_job:\n\n - build_statics_job: \n\n - deploy_job", //  text body
+    text: "Se ha realizado un push en la rama 'githubActions_improvement' que ha provocado la ejecución del workflow Bingo_Workflow con los siguientes resultados:- syntax_check_job: - test_execution_job: - build_statics_job: - deploy_job", //  text body
 
   }
 
-  console.log("Message sent: ", info['text']);
+  console.log("Message sent: ", info.text);
 
   transporter.sendMail(info, function(error, data){
     if (error) {
