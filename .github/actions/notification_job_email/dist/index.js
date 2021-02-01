2d6f6e5a794775da011770ff925e94ec3653e019
Alex Zarazua <alexzarazua88@gmail.com>
2021-02-01 09:41:02 +0100

    notification_job

--- .github/actions/notification_job_email/dist/index.js
+++ .github/actions/notification_job_email/dist/index.js
@@ -21,31 +21,26 @@ module.exports = JSON.parse("{\"_args\":[[\"nodemailer@6.4.17\",\"/home/usuario/
 /***/ 9211:
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __nccwpck_require__) => {
 
-
 let nodemailer = __nccwpck_require__(7170);
 let core = __nccwpck_require__(6066);
 
-
-let jobs = [
-    core.getInput("syntax_check_job"),
-    core.getInput("test_execution_job"),
-    core.getInput("build_statics_job"),
-    core.getInput("deploy_job")
-]
-
-console.log(jobs);
-console.log(jobs[0])
-console.log(jobs[1])
-console.log(jobs[2])
-console.log(jobs[3])
+const syntax_check_job = core.getInput("syntax_check_job");
+const test_execution_job = core.getInput("test_execution_job");
+const build_statics_job = core.getInput("build_statics_job");
+const deploy_job = core.getInput("deploy_job");
 
 
+
+let mail = core.getInput('NODE_EMAIL');
+let pass = core.getInput('THE_PASSWORD');
 // create reusable transporter object using the default SMTP transport
 var transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: "smtp.gmail.com", //host
+    port: 460,     //port
+    secure: true,  //secure
     auth: {
-        user: 'alexzarazua7email.test@gmail.com',
-        pass: core.getInput('NODE_EMAIL_PASSWORD')
+        user: mail,
+        pass: pass
     }
 });
 
@@ -56,24 +51,23 @@ function check(jobs) {
     return jobs;
 }
 
-
+let mail_author =  'alexzarazua7email.test@gmail.com';
+let to_send =  core.getInput('WORK_CLASS_EMAIL');
 
 // send mail with defined transport object
 let info = {
-    from: 'alexzarazua7email.test@gmail.com',// sender address
-    to: core.getInput('WORK_CLASS_EMAIL'), // list of receivers
+    from: mail_author,// sender address
+    to: to_send, // list of receivers
     subject: "Resultado del workflow ejecutado ✔", // Subject line
     text: `Se ha realizado un push en la rama "githubActions_improvement" que ha provocado la ejecución del workflow Bingo_Workflow 
-    con los siguientes resultados: \n\n\n
-    - syntax_check_job: ${check(jobs[0])}
-    - test_execution_job: ${check(jobs[1])}
-    - build_statics_job:  ${check(jobs[2])}
-    - deploy_job: ${check(jobs[3])}`       //  text body
-
-
+    con los siguientes resultados: \n\n\
+    - syntax_check_job: ${check(syntax_check_job)}  ✔
+    - test_execution_job: ${check(test_execution_job)} ✔
+    - build_statics_job:  ${check(build_statics_job)} ✔ 
+    - deploy_job: ${check(deploy_job)} ✔`        //  text body
 }
 
-console.log("Message sent: ", info.text,jobs[0]);
+console.log("Message sent: ", info.text);
 
 transporter.sendMail(info, function (error, data) {
     if (error) {
